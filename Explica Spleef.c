f732b50896310185dd912bae9e750800e6278878
Marcus <marcusfonseca15@gmail.com>
2024-06-24 22:32:40 -0300

    segundo commit

---
+++ Explica Spleef.c
@@ -0,0 +1,9 @@
+/*Predio 5x5x10 (10 andares)
+Cada andar tem um "buraco", que é randomizado a cada inicialização.
+
+Uma "bola" vai andar em busca desse buraco (De 1 em 1), e em seguida o algoritmo
+calcula a distancia entre o buraco e a posição atual, até chegar nele (Zerar).
+
+> Sendo um passo a passo, se a distancia aumentar, ele volta 1 passo automaticamente (Se qualquer eixo aumenta).
+> Ao mudar de andar, ele fará esse teste sempre.
+Voce vai imprimir a Distancia Total percorrida (soma do quanto percorreu em cada andar*/
--- Predio Matriz.c
+++ Predio Matriz.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*Um prédio tem 5 andares e 9 quartos [3][3] por andar (45 quartos ao todo).
-Procure um 1 em torno de vários 0.
-Usuario vai colocar a coordenada como chute, e algoritmo diz a distancia entre o chute
-e a posição (desconsidere a diagonal)
-Dick: randomiza um numero 3 vezes([3][3][5]) para descobrir o valor 1*/
+
 int main(){
     int x = 5, y = 3, z = 3;
     int matriz[5][3][3] = {0};
