4a0d17965a9f4edf603dbe0d56603b058a78ac57
Marcus <marcusfonseca15@gmail.com>
2024-06-26 10:42:38 -0300

    Erro no calculo de distancia

--- Predio Matriz.c
+++ Predio Matriz.c
@@ -2,28 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
+int main() {
     int x = 5, y = 3, z = 3;
     int matriz[5][3][3] = {0};
-    int cX, cY, cZ, dif;
+    int cX, cY, cZ, dif = 0;
     
     srand(time(NULL));
-    
-    // Randomiza só o indice (posiçãoo)
     int randX = rand() % x;
     int randY = rand() % y;
     int randZ = rand() % z;
     
-    // Colocando o 1 na posição gerada
     matriz[randX][randY][randZ] = 1;
-/*
-    //Pede ao usuario
+
     printf("Digite as coordenadas (x, y, z): ");
-    scanf("%d %d %d", cX, cY, cZ);
+    scanf("%d %d %d", &cX, &cY, &cZ);
     
-        // Calcular a distancia
-        int dif = abs(cX - randX) + abs(cY - randY) + abs(cZ - randZ);
-        printf("Errou! Faltam %d passos\n", dif);*/
+    //distancia
+    dif = abs(cX - randX) + abs(cY - randY) + abs(cZ - randZ);
+    printf("Errou! Faltam %d passos\n", dif);
     
     int i, j, k, l, m, n;
     for (i=0; i<x; i++) {
