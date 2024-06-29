053c8f9c771046150cc198785ac82088decf2b78
Marcus <marcusfonseca15@gmail.com>
2024-06-29 19:17:50 -0300

    OFICIAL

--- Predio Matriz.c
+++ Predio Matriz.c
@@ -3,53 +3,50 @@
 #include <time.h>
 
 int main() {
+    // notas pessoais**
+    // abs retorna o valor absoluto de uma diferença, por exemplo 1-5 = -4
+    // abs retornará somente o 4.
+
+    // randomização da posição do 1
     int x = 5, y = 3, z = 3;
     int matriz[5][3][3] = {0};
     int cX, cY, cZ, dif = 0;
-    
-    srand(time(NULL));
-    int randX = rand() % x;
-    int randY = rand() % y;
-    int randZ = rand() % z;
-    
-    matriz[randX][randY][randZ] = 1;
 
-    printf("Digite as coordenadas (x, y, z): ");
-    scanf("%d %d %d", &cX, &cY, &cZ);
+    srand(time(NULL));
+    int randX = (rand() % x) + 1; // altura
+    int randY = (rand() % y) + 1; // linha     //adiciona +1 para que o valor randomizado inclua 0
+    int randZ = (rand() % z) + 1; // coluna
     
-    //distancia
-    int difX = (cX - randX), difY = (cY - randY), difZ = (cZ - randZ);
-    if(difX < 0){
-        difX = randX - cX;
+    matriz[randX - 1][randY - 1][randZ - 1] = 1;   //adicona -1 para que as coordenadas sejam 
+                                                   //ajustadas aos indices da matriz pq a matriz começa em 0
+
+    while (1) {
+        printf("Digite as coordenadas (x, y, z): ");
+        scanf("%d %d %d", &cX, &cY, &cZ);
+
+        // calculo distancia
+        dif = abs(cX - randX) + abs(cY - randY) + abs(cZ - randZ);
+
+        if (dif == 0) {
+            printf("Parabens! Voce acertou!!\n");
+            break;
+        } else {
+            printf("Errou! Faltam %d passos\n", dif);
+        }
     }
-    if(difY < 0){
-        difY = randY - cY;
-    }
-    if(difZ < 0){
-        difZ = randZ - cZ;
-    }
-    //dif = abs(cX - randX) + abs(cY - randY) + abs(cZ - randZ);
-    printf("Errou! Faltam %d passos\n", dif);
-    
-    int i, j, k, l, m, n;
-    for (i=0; i<x; i++) {
-        for (j=0; j<y; j++) {
-            for (k=0; k<z; k++) {
+
+    // imprime a matriz
+    int i, j, k;
+    for (i = 4; i >= 0; i--) { // Invertido pra imprimir direito
+        for (j = 0; j < y; j++) {
+            for (k = 0; k < z; k++) {
                 printf("%d ", matriz[i][j][k]);
-                if (matriz[i][j][k] == 1){ //Grava a posição atual do 1
-                	l = i;
-                	m = j;
-                	n = k;
-				}
             }
             printf("\n");
         }
         printf("\n");
     }
-    int difsoma = difX+difY+difZ;
-    //int ababa = abs(cX-l)+abs(cY-m)+abs(cZ-n);
-    printf ("%d %d %d", l+1, m+1, n+1); //Mostrar coordenadas
-    //printf ("\n%d passos", ababa);
-    printf("FALTAM %d", difsoma);
+
+    printf("Coordernada Real: %d, %d, %d\n", randX, randY, randZ); // Mostrar coordenadas reais
     return 0;
 }
